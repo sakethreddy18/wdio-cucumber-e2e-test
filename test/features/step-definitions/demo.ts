b22d276f7f7e9be1a17b04541b18d44177faee45
sakethreddy18 <gsakethreddy18@gmail.com>
2024-08-16 12:05:16 +0530

    learn : learned how to select checkboxes

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -31,7 +31,7 @@ Then(/^URL should match (.*)$/ , async function (expectedURL) {
 // web interactions
 
 Given(/^A web page is opened$/ , async function (){
-    await browser.url("/inputs");
+    await browser.url("/checkboxes");
     await browser.setTimeout({implicit: 15000 , pageLoad: 10000});
     await browser.maximizeWindow();
 })
@@ -44,18 +44,73 @@ When(/^Perform web interactions$/ , async function() {
      * 3. Click and type
      * 4. Slow typeing
      */
-    let num = 12345
-    let strNum = num.toString();
-    let ele = await $(`[type="number"]`);
-    // await ele.setValue(strNum);
-    await ele.click();
+    // let num = 12345
+    // let strNum = num.toString();
+    // let ele = await $(`[type="number"]`);
+    // // await ele.setValue(strNum);
+    // await ele.click();
 
-    for(let i = 0 ; i < strNum.length ; i++){
-        let chatStr = strNum.charAt(i)
-        await browser.pause(1000)
-        await browser.keys(chatStr);
+    // for(let i = 0 ; i < strNum.length ; i++){
+    //     let chatStr = strNum.charAt(i)
+    //     await browser.pause(1000)
+    //     await browser.keys(chatStr);
+    // }
+    // await browser.pause(8000);
+
+    /**
+     * 2. Dropdown
+     * Actions
+     * 1. Assert default option is selected
+     * 2. Select by attribute , text , index
+     * 3. Get a list of options
+     */
+
+    // let ele =  await $('//select/option[@selected="selected"]');
+    // let val = await ele.getText();
+    // console.log(val)
+    // chai.expect(val).to.equal("Please select an option");
+   
+
+
+    // Select a specific value
+    // let dropDownOPtions = await $$('//select/option');
+    // let optionsArr = ['Please select an option' , 'Option 1' , 'Option 2']
+    // for (let i = 0 ; i < optionsArr.length ; i++){
+    //     console.log(await dropDownOPtions[i].getText())
+    //     chai.expect(await dropDownOPtions[i].getText()).to.equal(optionsArr[i])
+    // }
+
+    /**
+     * 3. Checkbox
+     * Actions:
+     * 1. Select an option
+     * 2. Unselect an option
+     * 3. Assert an option
+     * 4. Select all options
+     * 
+     */
+
+    let checkBoxes = await $$(`//form[@id="checkboxes"]/input`);
+    chai.expect(await checkBoxes[1].isSelected()).to.be.true;  //asserting an option
+    await checkBoxes[0].click(); //selecting the 1st option
+    await checkBoxes[1].click(); //unselecting the 2nd option
+    chai.expect(await checkBoxes[0].isSelected()).to.be.true; // asserting an option
+
+    //asserting all the checkboxes
+    for (let i = 0 ; i < checkBoxes.length ; i++){
+        console.log(await checkBoxes[i].getAttribute('value'));
+        if(!await checkBoxes[i].isSelected()){
+            await checkBoxes[i].click()
+        }
     }
-    await browser.pause(8000);
+    checkBoxes.forEach(async ele =>{
+        chai.expect(await ele.isSelected()).to.be.true
+    })
+
+    await browser.pause(3000) 
+
+
+
 
 })
 
