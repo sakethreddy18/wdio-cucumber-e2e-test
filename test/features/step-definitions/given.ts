daed5cd9c9e3f1538d9c406a51e61342e2985917
sakethreddy18 <gsakethreddy18@gmail.com>
2024-08-21 17:24:12 +0530

    learn : reload and refreshing use cases

--- test/features/step-definitions/given.ts
+++ test/features/step-definitions/given.ts
@@ -1,23 +1,53 @@
 import { Given } from "@wdio/cucumber-framework";
 import chai from "chai";
 import dotenv from "dotenv";
-Given(/^Login to inventory web app$/ , async function () {
-    /** 1. Launch the browser */
-    await browser.url("https://www.saucedemo.com/")
-    await browser.setTimeout({implicit : 15000 , pageLoad: 10000})
+Given(/^Login to inventory web app$/, async function () {
+  /** 1. Launch the browser */
+  await browser.url("https://www.saucedemo.com/");
 
-    /**2. Set the username and password */
+  /**2. Set the username and password */
 
-    let userInput = await $("#user-name");
+  try {
+    let userInput = await $("#user-nam");
     let passwordInput = await $("#password");
-    let loginButton = await $("#login-button")
+    let loginButton = await $("#login-button");
 
     const userName = process.env.USERNAME;
     const password = process.env.PASSWORD;
-    console.log(">>GIven block...")
+    console.log(">>GIven block...");
 
     await userInput.setValue(userName);
     await passwordInput.setValue(password);
     await loginButton.click();
-})
+  } catch (err) {
+    console.log(`Error in first login . Retrying..`);
+    await browser.refresh();
+    await browser.pause(2000);
+    let userInput = await $("#user-name");
+    let passwordInput = await $("#password");
+    let loginButton = await $("#login-button");
 
+    const userName = process.env.USERNAME;
+    const password = process.env.PASSWORD;
+    console.log(">>GIven block...");
+
+    await userInput.setValue(userName);
+    await passwordInput.setValue(password);
+    await loginButton.click();
+  }
+
+  // Login with another user
+  //   await browser.pause(2000);
+  //   await browser.reloadSession();
+  //   await browser.url("https://www.saucedemo.com/");
+  //   userInput = await $("#user-name");
+  //   passwordInput = await $("#password");
+  //   loginButton = await $("#login-button");
+
+  //   const probUser = process.env.PR_USERNAME;
+  //   await userInput.setValue(probUser);
+  //   await passwordInput.setValue(password);
+  //   await loginButton.click();
+
+  await browser.pause(3000);
+});
